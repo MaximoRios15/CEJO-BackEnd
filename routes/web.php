ac8ad42dfff2d1eb8585867bbebcca33824bcfac
Bruno <ramosbruno24424@gmail.com>
2025-08-22 18:51:04 -0300

    acciones malas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('admin/dashboard', AdminController::class)->middleware('auth','role:admin' );
